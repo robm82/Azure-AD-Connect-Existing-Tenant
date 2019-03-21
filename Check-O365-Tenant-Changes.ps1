d4f62c27623f118141c80de9e376eb858d3c5f95
robm82 <Robert.Milner@italik.co.uk>
2019-03-21 17:01:42 +0000

    Further changes and improvements

--- Check-O365-Tenant-Changes.ps1
+++ Check-O365-Tenant-Changes.ps1
@@ -7,6 +7,11 @@
     1.0.0       20/02/2019      Initial version
 #>
 
+Param (
+    [Parameter( Mandatory=$false )]
+    [string]$ADSearchBase
+)
+
 # LOGGING TO THE SCREEN AND TO FILE
 
 if (!(Test-Path -Path "C:\Temp"))
@@ -29,22 +34,33 @@ LogScreen "Working Directory: $($currDir)"
 
 # Connect to Office 365
 # Get list of domains
-$o365Domains = Get-MsolDomain -Status Verified
+$o365Domains = Get-MsolDomain -Status Verified | Select-Object -ExpandProperty Name
 
 # Get list of all users in local AD with a UPN:
 Import-Module ActiveDirectory
-$ADUsers = Get-ADUser -Filter * -SearchBase "OU=Highfield,OU=Staff,DC=HIGHBROOK,DC=local"
+
+if ($ADSearchBase)
+{
+    $ADUsers = Get-ADUser -Filter * -SearchBase $ADSearchBase
+}
+else
+{
+    $ADUsers = Get-ADUser -Filter *
+}
 
 foreach ($ADUser in $ADUsers)
 {
-    $userUPN = Get-ADUser $ADUser -Properties UserPrincipalName | Select-Object -Expand UserPrincipalName
-    if (!$UPNUser)
+    Write-Host "Processing user account $($ADuser.Name) ($($ADUser))" -ForegroundColor Magenta
+    
+    # Check if the user has a UPN assigned, and if it matches the domain in Office 365
+    $userUPN = Get-ADUser $ADUser | Select-Object -ExpandProperty UserPrincipalName
+    if (!$userUPN)
     {
-        LogScreen "ERROR: $($ADUser) does not have a UPN assigned"
+        Write-Host "ERROR: User account $($ADuser.Name) does not have a UPN assigned within AD" -ForegroundColor Red
     }
     else
     {
-        LogScreen "$($UPNUser) has a UPN assigned: $($userUPN)"
+        Write-Host "MATCH: User account $($ADuser.Name) has a UPN assigned: $($userUPN) within AD" -ForegroundColor Green
 
         # Now we need to check if the users UPN matches a verified domain within O365
         $userdomain = $userUPN.split("@",2)
@@ -54,32 +70,47 @@ foreach ($ADUser in $ADUsers)
         {
             if ($o365Domain -eq $userdomain)
             {
-                LogScreen "MATCH: User UPN matches a verified domain within Office 365"
+                Write-Host "MATCH: User account $($ADuser.Name) UPN ($($userdomain)) matches a verified domain ($($o365Domain)) within Office 365" -ForegroundColor Green
             }
             else
             {
-                LogScreen "ERROR: User UPN does not match a verified domain within Office 365"    
+                Write-Host "ERROR: User account $($ADuser.Name) does not match a verified domain within Office 365" -ForegroundColor Red
             }
         }
     }
-    $proxyAddresses = Get-ADUser $ADUser -Properties proxyAddresses | Select-Object -Expand proxyAddresses
+
+    # Check if the user has a Proxy Address, and see if the primary Proxy Address matches the UPN
+    $proxyAddresses = Get-ADUser $ADUser -properties * | Select-Object -ExpandProperty proxyAddresses
     if (!$proxyAddresses)
     {
-        LogScreen "ERROR: $($ADUser) does not have a Proxy Address assigned"
+        Write-Host "ERROR: User account $($ADuser.Name) does not have a Proxy Address assigned within AD" -ForegroundColor Red
     }
     else
     {
-        $PrimarySMTP = Get-ADUser $user -Properties proxyAddresses | Select-Object -Expand proxyAddresses | Where-Object {$_ -clike "SMTP:*"}
+        $PrimarySMTP = Get-ADUser $ADUser -properties * | Select-Object -ExpandProperty proxyAddresses | Where-Object {$_ -clike "SMTP:*"}
 
-        LogScreen "$($ADUser) has a Proxy Address assigned"
+        Write-Host "MATCH: User account $($ADuser.Name) has a proxy address assigned within AD" -ForegroundColor Green
         foreach ($proxyAddress in $proxyAddresses)
         {
-            LogScreen "$($proxyAddress)"
+            Write-Host "$($proxyAddress)" -ForegroundColor Cyan
         }
 
-        if (!($PrimarySMTP -eq $userUPN))
+        $PrimarySMTP = $PrimarySMTP.split(":",2)
+        $PrimarySMTP = $PrimarySMTP[1]
+        if ($PrimarySMTP -eq $userUPN)
         {
-            LogScreen "ERROR: Users primary SMTP address $($PrimarySMTP) does not match UPN $($userUPN)"
+            Write-Host "MATCH: User account $($ADuser.Name) primary SMTP address $($PrimarySMTP) matches UPN $($userUPN)" -ForegroundColor Green
+        }
+        else
+        {
+            Write-Host "ERROR: User account $($ADuser.Name) primary SMTP address $($PrimarySMTP) does not match UPN $($userUPN)" -ForegroundColor Red
         }
     }
+
+    # Check the user in Office 365 for the UPN and check it again AD
+    # Azure AD will softmatch the user against the UPN, ProxyAddresses listed in AD
+    Get-AzureADUser -ObjectId $PrimarySMTP | Select-Object -ExpandProperty UserPrincipalName
+
+    # Blank line at the bottom to split up the users as we log to the screen
+    Write-Host ""
 }
